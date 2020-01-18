9b8bfda5eab1e8108c50261e978c401a1ab276f5
jfsubrini <jfsubrini@zoho.com>
2020-01-18 22:37:57 +0100

    Working on the monitoring balance.

--- monitoring/currency_api.py
+++ monitoring/currency_api.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# pylint: disable=missing-class-docstring,too-few-public-methods,unnecessary-comprehension
+# pylint: disable=too-few-public-methods
 """Connection to the Currency Converter Restful API to get the currencies exchange rates."""
 
 import requests
@@ -8,9 +8,9 @@ from .config import CURRCONV_API_KEY
 
 
 class CurrencyConverter:
-    """ Class definition finding the coordinates of the place to find
-    and returning the latitude, longitude and the address of that place.
-    """  # TODO
+    """
+    Getting the currency exchange rate by requesting the Currency Converter Restful API.
+    """
 
     def __init__(self, currency_in, currency_out, date):
         self.currency_in = currency_in
@@ -32,13 +32,3 @@ class CurrencyConverter:
         curr_conv = response.json()
         rate = curr_conv[currencies][self.date]
         return rate
-
-
-if __name__ == "__main__":
-    # CurrencyConverter instance creation.
-    EXCHANGE_RATE = CurrencyConverter("EUR", "PEN", "2020-01-17")
-    RATE = EXCHANGE_RATE.exchange()
-    print("RATE = ", RATE)
-    # /api/v7/currencies?apiKey=[YOUR_API_KEY]
-    # /api/v7/convert?q=USD_PHP,PHP_USD&compact=ultra&date=[yyyy-mm-dd]&apiKey=[YOUR_API_KEY]
-    # /api/v7/convert?q=USD_PHP,PHP_USD&compact=ultra&date=[yyyy-mm-dd]&endDate=[yyyy-mm-dd]&apiKey=[YOUR_API_KEY]
--- monitoring/views.py
+++ monitoring/views.py
@@ -1,15 +1,44 @@
 # -*- coding: utf-8 -*-
-# pylint: disable=
+# pylint: disable=no-member
 """All the views for the monitoring app of the travel_budgeter project."""
 
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from draft.models import Draft
+from expenses.models import Expense
+from .currency_api import CurrencyConverter
+
 
 @login_required(login_url="/signin/", redirect_field_name="redirection_vers")
 def monitoring(request):
     """
-    View to the monitoring page.
+    View to the monitoring balance page.
     """
-    context = {}
-    return render(request, "monitoring.html", context)
+    last_draft = Draft.objects.filter(user=request.user).last()
+    wallet_queryset = last_draft.wallets.all()
+    wallet_dict = {}
+    for wallet in wallet_queryset:
+        initial_balance = wallet.balance
+        wallet_currency = wallet.currency
+        expenses_related_queryset = Expense.objects.filter(
+            wallet=wallet, draft=last_draft
+        )
+        expense_amount_list = []
+        for expense in expenses_related_queryset:
+            # TODO pour crédit et pas que dépenses
+            expense_amount = expense.amount
+            expense_currency = expense.currency.iso
+            if wallet_currency != expense_currency:
+                expense_date = expense.date
+                currency_rate = CurrencyConverter(
+                    wallet_currency, expense_currency, expense_date
+                ).exchange()  # TODO Attention pb car les dates sont dans le futur. Refaire les expenses avec dates dans le passé.
+                expense_amount *= currency_rate
+            expense_amount_list.append(expense_amount)
+        wallet_balance = initial_balance - sum(expense_amount_list)
+        wallet_dict[wallet.id] = wallet_balance
+
+    context = {"wallet_dict": wallet_dict}
+    return render(request, "balance.html", context)
+
